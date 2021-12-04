29cf049642fce213cbe2d74673f1cbd244fb98e4
aixed <364831018@qq.com>
2021-12-04 23:02:25 +0800

    20211204

--- database_config.py
+++ database_config.py
@@ -1,11 +1,17 @@
 
 
 # 配置 A++ 6.6 数据库连接信息
+# HOST = '10.11.0.37'
+# PORT = '5324'
+# USERNAME = 'xzsijjcw2019'
+# PASSWORD = 'xzsijjcw2021'
+# DATABASE = 'xzrsjjcw'
+
 HOST = '10.11.0.37'
-PORT = '5324'
-USERNAME = 'xzsijjcw2019'
-PASSWORD = 'xzsijjcw2021'
-DATABASE = 'xzrsjjcw'
+PORT = '5212'
+USERNAME = 'AIXED_20211111'
+PASSWORD = 'xzsijjcw'
+DATABASE = 'xzrszjk'
 
 SQLALCHEMY_DATABASE_URI = "oracle://{username}:{password}@{host}:{port}/{db}".format(username=USERNAME,
                                                                                         password=PASSWORD,
